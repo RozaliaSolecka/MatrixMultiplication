5329246b2d65fa94e414505c2ea693c4f6dd537a
Rozalia Solecka <rozaliasolecka@gmail.com>
2022-11-25 22:45:21 +0100

    v4

--- openmp.c
+++ openmp.c
@@ -4,112 +4,95 @@
 #include <sys/time.h>
 #include <omp.h>
 #include <math.h>
-#define PRECISION 0.000001
-#define RANGESIZE 1
 
-void initializeMatrixes(int* A, int* B, int* C, int DIMENSION, int RAND_RANGE) {
-  for (int i=  0; i < DIMENSION; i++) {
-        for (int j = 0; j < DIMENSION; j++) {
-			int offset = i * DIMENSION + j;
-            A[offset] = (rand() % (RAND_RANGE + 1));
-            B[offset] = (rand() % (RAND_RANGE + 1));
-            C[offset] = 0;
-		}
-	}
+void initializeMatrixes(int** A, int** B, int** C, int DIMENSION, int RAND_RANGE, int THREAD_NUM) {
+    int i, j;
+    omp_set_num_threads(THREAD_NUM);
+    #pragma omp parallel for private(i,j) shared(A,B,C)
+    for (i = 0; i < DIMENSION; i++) {
+        for (j = 0; j < DIMENSION; j++) {
+            A[i][j] = (rand() % (RAND_RANGE + 1));
+            B[i][j] = (rand() % (RAND_RANGE + 1));
+            C[i][j] = 0;
+        }
+    }
 }
 
-void clearResults(int* C, int DIMENSION) {
-  for (int i = 0; i < DIMENSION; i++) {
-        for (int j = 0; j < DIMENSION; j++) {
-			int offset = i * DIMENSION + j;
-            C[offset] = 0;
-		}
-	}
+void clearResults(int** C, int DIMENSION, int THREAD_NUM) {
+    int i, j;
+    omp_set_num_threads(THREAD_NUM);
+    #pragma omp parallel for private(i,j) shared(C)
+    for (i = 0; i < DIMENSION; i++) {
+        for (j = 0; j < DIMENSION; j++) {
+            C[i][j] = 0;
+        }
+    }
 }
 
-void multiplyMatrixes_1(int* A, int* B, int* C, int DIMENSION) {
+void multiplyMatrixes_1(int** A, int** B, int** C, int DIMENSION, int THREAD_NUM) {
     double elapsed;
     struct timeval tv1, tv2;
     struct timezone tz;
     int i, j, k;
-    gettimeofday(&tv1, &tz);
+    double start, end;
+    omp_set_num_threads(THREAD_NUM);
+    start = omp_get_wtime();
 	
     #pragma omp parallel for private(i,j,k) shared(A,B,C)
     for (i = 0; i < DIMENSION; i++) {
         for (j = 0; j < DIMENSION; j++) {
             for (k = 0; k < DIMENSION; k++) {
-				int offset = i * DIMENSION + j;
-				int index1 = i * DIMENSION + k;
-				int index2 = k * DIMENSION + j;
-                C[offset] += A[index1] * B[index2];
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 
-    gettimeofday(&tv2, &tz);
+    end = omp_get_wtime();
 	
-    elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
-    printf("Algorithm 1 - elapsed time = %f microseconds.\n", elapsed);
+    elapsed = (double) (end-start) + (double) (end-start) * 1.e-6;
+    printf("Algorithm 2 - elapsed time = %f seconds.\n", elapsed);
 }
 
-void multiplyMatrixes_2(int* A, int* B, int* C, int DIMENSION) {
+void multiplyMatrixes_2(int** A, int** B, int** C, int DIMENSION, int THREAD_NUM) {
     double elapsed;
     struct timeval tv1, tv2;
     struct timezone tz;
-    int i, j, k;
-    memset(C, 0, sizeof C);
-    gettimeofday(&tv1, &tz);
-	
-    #pragma omp parallel for schedule(static,50) collapse(2) private(i,j,k)shared(A, B, C)
-    for (i = 0; i < DIMENSION; i++) {
-        for (j = 0; j < DIMENSION; j++) {
-            for (k = 0; k < DIMENSION; k++) {
-				int offset = i * DIMENSION + j;
-				int index1 = i * DIMENSION + k;
-				int index2 = k * DIMENSION + j;
-                C[offset] += A[index1] * B[index2];
+    int i, j;
+    double start, end;
+    
+    # pragma omp parallel num_threads(THREAD_NUM) default(none) shared(A,B,C,start) private(i,j) firstprivate(DIMENSION)
+    {
+        # pragma omp single
+        {
+        start = omp_get_wtime();
+        }
+        # pragma omp single
+        {
+            for (i = 0; i < DIMENSION; i++) {
+                for (j = 0; j < DIMENSION; j++) {
+                    # pragma omp task firstprivate(i,j)
+                    {
+                        int k;
+                        for (k = 0; k < DIMENSION; k++) {
+                            C[i][j] += A[i][k] * B[k][j];
+                        }
+                    }
+                }
             }
         }
     }
 
-    gettimeofday(&tv2, &tz);
+    end = omp_get_wtime();
 	
-    elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
-    printf("Algorithm 2 - elapsed time = %f microseconds.\n", elapsed);
+    elapsed = (double) (end-start) + (double) (end-start) * 1.e-6;
+    printf("Algorithm 1 - elapsed time = %f seconds.\n", elapsed);
 }
 
-void multiplyMatrixes_3(int* A, int* B, int* C, int DIMENSION) {
-    double elapsed;
-    struct timeval tv1, tv2;
-    struct timezone tz;
-    int i, j, k;
-    memset(C, 0, sizeof C);
-    gettimeofday(&tv1, &tz);
-	
-    #pragma omp parallel for schedule(dynamic,50) collapse(2) private(i,j,k) shared(A, B, C)
-    for (i = 0; i < DIMENSION; i++) {
-        for (j = 0; j < DIMENSION; j++) {
-            for (k = 0; k < DIMENSION; k++) {
-				int offset = i * DIMENSION + j;
-				int index1 = i * DIMENSION + k;
-				int index2 = k * DIMENSION + j;
-                C[offset] += A[index1] * B[index2];
-            }
-        }
-    }
-
-    gettimeofday(&tv2, &tz);
-	
-    elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
-    printf("Algorithm 3 - elapsed time = %f microseconds.\n", elapsed);
-}
-void printMatrixes(int* A, int* B, int* C, int DIMENSION) {
-    int offset;
+void printMatrixes(int** A, int** B, int** C, int DIMENSION) {
 	printf("First input matrix \n");
     for (int i = 0; i < DIMENSION; i++) {
         for (int j = 0; j < DIMENSION; j++) {
-			offset = i * DIMENSION + j;
-            printf("%d\t",A[offset]);
+            printf("%d\t",A[i][j]);
         }
         printf("\n");
     }
@@ -117,8 +100,7 @@ void printMatrixes(int* A, int* B, int* C, int DIMENSION) {
 	printf("Second input matrix \n");
 	for (int i = 0; i < DIMENSION; i++) {
         for (int j = 0; j < DIMENSION; j++) {
-			offset = i * DIMENSION + j;
-            printf("%d\t",B[offset]);
+            printf("%d\t",B[i][j]);
         }
         printf("\n");
     }
@@ -126,8 +108,7 @@ void printMatrixes(int* A, int* B, int* C, int DIMENSION) {
 	printf("Result \n");
 	for (int i= 0; i < DIMENSION; i++) {
         for (int j = 0; j < DIMENSION; j++) {
-			offset = i * DIMENSION + j;
-            printf("%d\t",C[offset]);
+            printf("%d\t",C[i][j]);
         }
         printf("\n");
     }
@@ -149,22 +130,31 @@ int main(int argc,char **argv) {
 	//program input argument
 	int DIMENSION = ins__args.start; 
 	int RAND_RANGE = ins__args.stop;
+    int THREAD_NUM = ins__args.n_thr;
 
-	int *A = (int *)malloc(DIMENSION * DIMENSION * sizeof(int));
-	int *B = (int *)malloc(DIMENSION * DIMENSION * sizeof(int));
-	int *C = (int *)malloc(DIMENSION * DIMENSION * sizeof(int));
+	int **A = (int **)malloc(DIMENSION * sizeof(int*));
+	int **B = (int **)malloc(DIMENSION * sizeof(int*));
+	int **C = (int **)malloc(DIMENSION * sizeof(int*));
+
+    for (int i = 0; i < DIMENSION; i++) {
+        A[i] = (int *)malloc(DIMENSION * sizeof(int));
+        B[i] = (int *)malloc(DIMENSION * sizeof(int));
+        C[i] = (int *)malloc(DIMENSION * sizeof(int));
+    }
 	
-	initializeMatrixes(A, B, C, DIMENSION, RAND_RANGE);
-    multiplyMatrixes_1(A, B, C, DIMENSION);
+	initializeMatrixes(A, B, C, DIMENSION, RAND_RANGE, THREAD_NUM);
+    multiplyMatrixes_1(A, B, C, DIMENSION, THREAD_NUM);
     //printMatrixes(A, B, C, DIMENSION);
 
-    clearResults(C, DIMENSION);
-    multiplyMatrixes_2(A, B, C, DIMENSION);
+    clearResults(C, DIMENSION, THREAD_NUM);
+    multiplyMatrixes_2(A, B, C, DIMENSION, THREAD_NUM);
     //printMatrixes(A, B, C, DIMENSION);
 
-    clearResults(C, DIMENSION);
-    multiplyMatrixes_3(A, B, C, DIMENSION);
-    //printMatrixes(A, B, C, DIMENSION);
+    for (int i = 0; i < DIMENSION; i++) {
+        free(A[i]);
+        free(B[i]);
+        free(C[i]);
+    }
     
     free(A);
     free(B);
